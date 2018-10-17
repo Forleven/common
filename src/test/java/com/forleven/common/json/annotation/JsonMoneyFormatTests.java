0540caf7ea781f12e82e557fb891ea962eae4476
Renan Palmeira <renanpalmeira1@hotmail.com>
2018-10-17 16:32:15 -0300

    Fixed error in class name

--- src/test/java/com/forleven/common/json/annotation/JsonMoneyFormatTests.java
+++ src/test/java/com/forleven/common/json/annotation/JsonMoneyFormatTests.java
@@ -19,7 +19,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 @RunWith(MockitoJUnitRunner.class)
-public class jsonMoneyFormatTests {
+public class JsonMoneyFormatTests {
 
     @AllArgsConstructor
     @NoArgsConstructor
