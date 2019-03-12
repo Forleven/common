adc0e0d4b29659623c31f652442ae9c7d5f2f7fe
Renan Palmeira <renanpalmeira1@hotmail.com>
2019-03-12 09:02:37 -0300

    Add @AllArgsConstructor in Link

--- src/main/java/com/forleven/common/web/Link.java
+++ src/main/java/com/forleven/common/web/Link.java
@@ -1,9 +1,11 @@
 package com.forleven.common.web;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 @Builder
 public class Link {
     private String href;
